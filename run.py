70b1203b85cc6f04e158548f25540c88c593e940
sganeshcv <sreeganesh.360@gmail.com>
2020-11-09 13:46:28 +0530

    file upload and display done

--- run.py
+++ run.py
@@ -3,14 +3,26 @@ from pymongo import MongoClient
 from bson import ObjectId
 from flask_login import login_required, current_user, login_user, logout_user
 from models import login, UserModel
+import os
+from flask_socketio import SocketIO, emit
+from flask import session
+
 # config system
-app = Flask(__name__)
+app = Flask(__name__, static_folder="images")
 app.config.update(dict(SECRET_KEY='yoursecretkey'))
 client = MongoClient('localhost:27017')
 db = client.Image
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 login.init_app(app)
 login.login_view = 'loginPage'
+
+socketio = SocketIO(app)
+
+@socketio.on('disconnect')
+def disconnect_user():
+    logout_user()
+    session.pop('yourkey', None)
 
 @app.route("/create", methods = ['POST'])
 @login_required
@@ -24,9 +36,30 @@ def create():
     value2=request.values.get("value2")
     key3=request.values.get("key3")
     value3=request.values.get("value3")
-    entry = {"name":image_name, key1:value1, key2:value2, key3:value3}
-    db.ImageEntry.insert_one(entry)
-    return redirect('/listall')
+
+    target = os.path.join(APP_ROOT,'images/')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    for upload in request.files.getlist("img"):
+        filename = image_name
+        # This is to verify files are supported
+        ext = os.path.splitext(filename)[1]
+        if (ext == ".jpg") or (ext == ".png"):
+            print("File supported moving on...")
+        else:
+            return render_template('create.html')
+        destination = "/".join([target, filename])
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
+
+    if image_name:
+        entry = {"name":image_name, key1:value1, key2:value2, key3:value3}
+        db.ImageEntry.insert_one(entry)
+        return redirect('/listall')
+
+    else:
+        return render_template('create.html')
 
 @app.route("/create", methods = ['GET'])
 @login_required
@@ -40,13 +73,19 @@ def createGET():
 def search():
     key1=request.values.get("key1")
     value1=request.values.get("value1")
-    key2=request.values.get("key2")
-    value2=request.values.get("value2")
-    key3=request.values.get("key3")
-    value3=request.values.get("value3")
-    entry = {key1:value1, key2:value2, key3:value3}
-    db.ImageEntry.insert_one(entry)
-    return render_template('results.html')
+    # key2=request.values.get("key2")
+    # value2=request.values.get("value2")
+    # key3=request.values.get("key3")
+    # value3=request.values.get("value3")
+    # entry = {key1:value1, key2:value2, key3:value3}
+    entry = {key1:value1}
+    results = db.ImageEntry.find(entry)
+    # for result in results:
+    #     # target = os.path.join(APP_ROOT,'images/')
+    #     # destination = "".join([target, result.get('name')])
+    #     # names.append(destination)
+    #     names.append(result.get('name'))
+    return render_template('results.html', data = results)
 
 @app.route('/authenticate', methods = ['GET', 'POST'])
 def authenticate():
@@ -57,9 +96,8 @@ def authenticate():
     if(psw == "root" and uname == "root"):
         login_user(user)
         return render_template('adminHome.html')
-
-    # elif(userType == "guest" and uname == "guest" and psw == "guest"):
-    #     return render_template('searchPage.html')
+    elif(userType == "guest" and uname == "guest" and psw == "guest"):
+        return render_template('searchPage.html')
 
     return render_template('error.html')
 
